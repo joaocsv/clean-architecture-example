b43f88b6917cdae4d4eaabce8e1dfcb8338b8cc7
João Carlos <joaoc.avils@gmail.com>
2022-08-29 13:45:54 -0300

    feat: construido a implementação do repositorio de produto

--- src/infrastructure/db/sequelize/repository/product.repository.ts
+++ src/infrastructure/db/sequelize/repository/product.repository.ts
@@ -4,21 +4,35 @@ import ProductModel from '../model/product.model'
 
 export default class ProductRepository implements ProductRepositoryInterface {
   async create(entity: Product): Promise<void> {
-    ProductModel.create({
+    await ProductModel.create({
       id: entity.id,
       name: entity.name,
       price: entity.price
     })
   }
   
-  update(entity: Product): Promise<void> {
-    throw new Error('Method not implemented.')
-  }
-  find(id: string): Promise<Product> {
-    throw new Error('Method not implemented.')
-  }
-  findAll(): Promise<Product>[] {
-    throw new Error('Method not implemented.')
+  async update(entity: Product): Promise<void> {
+    await ProductModel.update({
+      name: entity.name,
+      price: entity.price
+    }, {
+      where: {
+        id: entity.id
+      }
+    })
   }
   
+  async find(id: string): Promise<Product> {
+    const productModel = await ProductModel.findOne({ where: { id }})
+    
+    return new Product(productModel.id, productModel.name, productModel.price)  
+  }
+
+  async findAll(): Promise<Product[]> {
+    const productsModel = await ProductModel.findAll()
+    
+    return productsModel.map(productModel => {
+      return new Product(productModel.id, productModel.name, productModel.price)
+    })
+  }  
 }
